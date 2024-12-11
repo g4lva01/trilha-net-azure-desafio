81c43a2da9b99b1afd3254b07c8dd1371aa5204c
g4lva01 <gabrielgalvao010@gmail.com>
2024-12-11 09:42:43 -0300

    Finalização do desafio

---
+++ Migrations/20241020201034_FirstMigration.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace trilha_net_azure_desafio.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
